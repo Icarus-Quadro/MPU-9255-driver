71ee104cd88e54116048cf8e969f5aaba816bf82
Roman Chomik <romanchom@gmail.com>
2018-12-02 20:03:36 +0100

    Added more registers for AccelGyro. Enabled direct access to magnetometer. Added magnetometer measurement register.

--- Registers.hpp
+++ Registers.hpp
@@ -2,47 +2,88 @@
 
 #include "Types.hpp"
 
-class VectorRegister
+namespace AccelerometerGyroscope {
+enum ActiveLevel : uint8_t
 {
-public:
-    float x() const
-    {
-        return static_cast<float>(mX.value());
-    }
-
-    float y() const
-    {
-        return static_cast<float>(mY.value());
-    }
-
-    float z() const
-    {
-        return static_cast<float>(mZ.value());
-    }
-private:
-    BigEndian<int16_t> mX;
-    BigEndian<int16_t> mY;
-    BigEndian<int16_t> mZ;
+    high,
+    low,
 };
 
-class TemperatureRegister {
-public:
+enum PinMode : uint8_t 
+{
+    pushPull,
+    openDrain, 
+};
+
+struct TemperatureRegister : Register::Scalar<int16_t, Register::Endian::big>
+{
     enum { address = 65 };
-    float value() const
-    {
-        return static_cast<float>(mValue.value()) / 333.87f + 21.0f;
-    }
-
-private:
-    BigEndian<int16_t> mValue;
 };
 
-struct AccelerometerMeasurements : VectorRegister
+struct AccelerometerMeasurements : Register::Vector<int16_t, 3, Register::Endian::big>
 {
     enum { address = 59 };
 };
 
-struct GyroscopeMeasurements : VectorRegister
+struct GyroscopeMeasurements : Register::Vector<int16_t, 3, Register::Endian::big>
 {
     enum { address = 67 };
 };
+
+struct InterruptBypassConfiguration
+{
+    enum { address = 55 };
+
+    uint8_t : 1;
+    bool enableBypass : 1;
+    bool enableFsyncInterrupt : 1;
+    ActiveLevel fsyncActiveLevel : 1;
+    bool clearInterruptOnRead : 1;
+    bool latchInterrputPin : 1;
+    PinMode interruptPinMode : 1;
+    ActiveLevel interruptPinLevel : 1;
+};
+}
+
+namespace Magnetometer {
+
+enum class OperationMode : uint8_t
+{
+    powerDown = 0b0000,
+    singleMeasurement = 0b0001,
+    continousMeasurement1 = 0b0010,
+    continousMeasurement2 = 0b0110,
+    externalTriggerMeasurement = 0b0100,
+    selfTest = 0b1000,
+    fuseRomAccess = 0b1111,
+};
+
+enum class OutputBitSetting : uint8_t
+{
+    bits14 = 0,
+    bits16 = 1,
+};
+
+struct Measurements : Register::Vector<int16_t, 3, Register::Endian::little>
+{
+    enum { address = 3 };
+
+    uint8_t : 3;
+    bool overflow : 1;
+    OutputBitSetting outputBit : 1;
+};
+
+struct Control1
+{
+    enum { address = 0xA };
+    OperationMode mode : 4;
+    OutputBitSetting outputBit : 1;
+};
+
+struct WhoAmI
+{
+    enum { address = 0 };
+    uint8_t data;
+};
+
+}
--- Types.hpp
+++ Types.hpp
@@ -3,23 +3,115 @@
 #include <cstdint>
 #include <cstddef>
 
-template<typename T>
-class BigEndian
+namespace Register
 {
-    static_assert(sizeof(T) == sizeof(uint16_t));
+namespace detail
+{
+    template<size_t SIZE>
+    struct UnsignedIntegerTrait;
+
+    template<>
+    struct UnsignedIntegerTrait<1>
+    {
+        using Type = uint8_t;
+    };
+
+    template<>
+    struct UnsignedIntegerTrait<2>
+    {
+        using Type = uint16_t;
+    };
+
+    template<>
+    struct UnsignedIntegerTrait<4>
+    {
+        using Type = uint32_t;
+    };
+
+    template<size_t SIZE>
+    using UnsignedInteger = typename UnsignedIntegerTrait<SIZE>::Type;
+}
+
+enum class Endian
+{
+    little,
+    big
+};
+
+template<typename T, Endian ENDIAN>
+class Scalar;
+
+template<typename T>
+class Scalar<T, Endian::little>
+{
 public:
     T value() const
     {
-        uint16_t swapped = (static_cast<uint16_t>(highByte) << 8)
-            | static_cast<uint16_t>(lowByte);
-        return reinterpret_cast<T&>(swapped);
-    }
-
-    operator T () const
-    {
-        return value();
+        return mData;
     }
 private:
-    uint8_t highByte;
-    uint8_t lowByte;
+    T mData;
 };
+
+template<typename T, size_t I>
+struct SwapBytes
+{
+    constexpr T operator()(uint8_t const* data)
+    {
+        return static_cast<T>(data[I - 1]) | SwapBytes<T, I - 1>()(data) << 8;
+    }
+};
+
+template<typename T>
+struct SwapBytes<T, 0>
+{
+    constexpr T operator()(uint8_t const*)
+    {
+        return 0;
+    }
+};
+
+template<typename T>
+class Scalar<T, Endian::big>
+{
+public:
+    T value() const
+    {
+        auto ret = partial<sizeof(T)>();
+        return reinterpret_cast<T&>(ret);
+    }
+private:
+    template<size_t i>
+    constexpr detail::UnsignedInteger<sizeof(T)> partial() const
+    {
+        return SwapBytes<detail::UnsignedInteger<sizeof(T)>, sizeof(T)>()(mData);
+    }
+    uint8_t mData[sizeof(T)];
+};
+
+template<typename T, size_t SIZE, Endian ENDIAN>
+class Vector
+{
+public:
+    T x() const
+    {
+        return mComponents[0].value();
+    }
+
+    T y() const
+    {
+        static_assert(SIZE >= 2, "Vector has to have SIZE >= 2");
+        return mComponents[1].value();
+    }
+
+    T z() const
+    {
+        static_assert(SIZE >= 3, "Vector has to have SIZE >= 3");
+        return mComponents[2].value();
+    }
+private:
+    using Component = Scalar<T, ENDIAN>;
+    Component mComponents[SIZE];
+};
+
+}
